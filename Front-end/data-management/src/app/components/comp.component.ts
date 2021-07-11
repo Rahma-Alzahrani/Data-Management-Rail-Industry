0600aaf313b280eb157685fada06a83164b51d7e
Rahma-Alzahrani <60695526+Rahma-Alzahrani@users.noreply.github.com>
2021-07-11 21:04:35 +0100

    Add files via upload

---
+++ Front-end/data-management/src/app/components/comp.component.ts
@@ -0,0 +1,42 @@
+import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'app-comp',
+  templateUrl: './comp.component.html',
+  styleUrls: ['./comp.component.scss']
+})
+export class CompComponent implements OnInit, OnDestroy {
+
+  mobileQuery: MediaQueryList;
+  userType: any;
+  userEmail: any;
+  private _mobileQueryListener: () => void;
+
+
+  constructor(
+    changeDetectorRef: ChangeDetectorRef,
+    media: MediaMatcher,
+    private router: Router
+  ) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  ngOnInit(): void {
+    this.userType = sessionStorage.getItem('uType');
+    this.userEmail = sessionStorage.getItem('uEmail');
+  }
+
+  logout() {
+    sessionStorage.clear();
+    localStorage.clear();
+    this.router.navigateByUrl('/auth/login');
+  }
+
+}
---
+++ Front-end/data-management/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  baseUrl: 'http://3.9.188.134:3000'
+};
